7ff44f681c3894847cd9761c41f2bbd8305412dd
jwlutz <jackwlutz1@gmail.com>
2025-11-10 02:07:47 -0600

    Fix deprecated titlefont_size in Plotly graph generation

--- visualizer.py
+++ visualizer.py
@@ -227,8 +227,7 @@ class SiteGraphBuilder:
         fig = go.Figure(
             data=[edge_trace, node_trace],
             layout=go.Layout(
-                title='Site Structure Graph',
-                titlefont_size=16,
+                title=dict(text='Site Structure Graph', font=dict(size=16)),
                 showlegend=False,
                 hovermode='closest',
                 margin=dict(b=20, l=5, r=5, t=40),
